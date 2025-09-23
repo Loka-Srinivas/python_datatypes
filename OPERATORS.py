54b341c54b152adf079b6d72f0833bcc8c2beb65
Loka-Srinivas <lokasrinivas@gmail.com>
2025-09-23 19:47:05 +0530

    python

---
+++ OPERATORS.py
@@ -0,0 +1,121 @@
+chocolates_i_have=10
+chocolates_i_eat=3
+chocolates_left=chocolates_i_have-chocolates_i_eat
+print(chocolates_left)
+
+x=10
+y=15
+print(x+y)
+
+x=10
+y=5
+print(x-y)
+
+x=10
+y=3
+print(x+y)
+
+# addition
+print("addition:",x+y)
+# substraction
+print("substaction:",x-y)
+# multiplication
+print("multiplication:",x*y)
+# division
+print("division:",x/y)
+# floor division
+print("floor division:",x//y)
+# modules
+print("modules:",x%y)
+# Exponential
+print("power:",x**y)
+
+
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(x**y)
+
+
+
+# Assignment Operators
+x=5
+
+print(x)
+# addition and assign
+x+=3
+print(x)
+# substaction and assign
+x-=3
+print(x)
+# multiplication and assign
+x*=5
+print(x)
+# division and assign
+x/=3
+print(x)
+# floordivision and assign
+x//=3
+print(x)
+# modulus and assign
+x%=3
+print(x)
+# exponsional and assign
+x**=3
+print(x)
+
+
+
+a=5
+b=4
+
+# equal to
+print(a==b)
+
+# not equal to
+print(a!=b)
+
+# greater than
+print(a>b)
+
+# less than
+print(a<b)
+
+# greater than or equal to
+print(a<=b)
+
+# less than or equal to
+print(a<=b)
+
+
+print(6>>2)
+print(6<<2)
+
+print(~5)
+
+print(~1)
+print(~2)
+print(~3)
+print(~4)
+print(~5)
+print(~6)
+print(~7)
+print(~8)
+print(~9)
+print(~10)
+
+
+print(3>2 and 5==5)
+print(5>6 and 6==5)
+print(8==9 and 5<8)
+
+
+print(5<4 or 3==2)
+print(5>4 or 3==2)
+
+
+print(5<6)
